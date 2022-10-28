df425861a8e7b7b0492487190716674b7f30595c
Matheus Tonon <math.tonon@gmail.com>
2022-10-28 07:38:20 -0300

    serving HTML files with Node & Express

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const express = require('express'); // imports require express with require function
+const path = require('path'); // build-in path package to construct routes
 
-const app = express(); // express is a function that we can execute
+const express = require('express');
+
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
-}); // a path (route) to  listen incoming request and 'do something'
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
+  });
 
-app.listen(3000); // to start listen for incoming request on a certain port
+app.get('/about', function (req, res) {
+  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+  res.sendFile(htmlFilePath);
+});
+
+app.get('/confirm', function (req, res) {
+  const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+  res.sendFile(htmlFilePath);
+});
+
+app.get('/recommend', function (req, res) {
+  const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+  res.sendFile(htmlFilePath);
+});
+
+app.get('/restaurants', function (req, res) {
+  const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+  res.sendFile(htmlFilePath);
+});
+
+
+app.listen(3000);
