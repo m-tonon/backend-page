c5d4d2a0d266318b124978adfe0966cd79530b69
Matheus Tonon <math.tonon@gmail.com>
2022-10-28 08:13:22 -0300

    Parsing form data & redirecting requests

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
+const fs = require('fs');
 const path = require('path'); // build-in path package to construct routes
 
 const express = require('express');
 
 const app = express();
 
-app.use(express.static('public')); // middleware to serve static files as CSS & JS
+app.use(express.static('public')); // middleware to serve static files as CSS & JS)
+app.use(express.urlencoded({extended: false})) 
 
 app.get('/', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    res.sendFile(htmlFilePath);
-  });
-
-app.get('/about', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-  res.sendFile(htmlFilePath);
-});
-
-app.get('/confirm', function (req, res) {
-  const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+  const htmlFilePath = path.join(__dirname, 'views', 'index.html');
   res.sendFile(htmlFilePath);
 });
 
@@ -26,10 +18,33 @@ app.get('/recommend', function (req, res) {
   res.sendFile(htmlFilePath);
 });
 
+app.post('/recommend', function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname,'data','restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm')
+});
+
+app.get('/confirm', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+  });
+
 app.get('/restaurants', function (req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
   res.sendFile(htmlFilePath);
 });
 
+app.get('/about', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
+  });
 
 app.listen(3000);
